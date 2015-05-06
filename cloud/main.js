ccc95e2bc4bd97ed7e3efef401a73d2b64fc7dd1
meowoodie <woodielove@163.com>
2015-05-06 17:14:01 +0800

    first commit

---
+++ cloud/main.js
@@ -0,0 +1,10 @@
+// Use AV.Cloud.define to define as many cloud functions as you want.
+// For example:
+
+
+AV.Cloud.define('senz_generator', function (request, response){
+    //method.senzGenerator(0);
+    //response.success('Senz Generator');
+});
+
+
