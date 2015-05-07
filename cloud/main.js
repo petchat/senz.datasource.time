71400dee236fd6298d3884db1a18dc082780ef37
meowoodie <woodielove@163.com>
2015-05-07 17:00:33 +0800

    add app.js and view

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,7 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 var calendar = require('cloud/calendar.js');
+require('cloud/app.js');
 
 AV.Cloud.define('debug', function (request, response){
     start_unixtime = request.params.start_unixtime;
@@ -20,3 +21,4 @@ AV.Cloud.define('today', function (request, response){
 
 //console.log(calendar.today(2015, 5, 21));
 //console.log(calendar.Day(2015, 5, 10));
+
